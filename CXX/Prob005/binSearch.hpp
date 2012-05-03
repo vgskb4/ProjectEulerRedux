7924658ad7f0131c1c15a0ff057f256be6dc09e4
Victor Smith <vgskb4@gmail.com>
2012-05-02 22:31:06 -0500

    Initial 12 working solutions from my prev repo

---
+++ CXX/Prob005/binSearch.hpp
@@ -0,0 +1,15 @@
+#ifndef _BIN_SEARCH_HPP_
+#define _BIN_SEARCH_HPP_
+
+typedef long long unsigned int myInt;
+
+myInt binSearch(const myInt &lower,const myInt &upper, myInt &mid){
+	mid=(lower+upper)/2;
+	if(meetsProblemCriteria){
+
+	}
+
+}
+
+
+#endif
