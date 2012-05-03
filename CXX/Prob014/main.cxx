9ff7d394c8529c963d0783ad2e9977b1fd9cb4a8
Victor Smith <vgskb4@gmail.com>
2012-05-02 23:53:47 -0500

    working solution

--- CXX/Prob014/main.cxx
+++ CXX/Prob014/main.cxx
@@ -1,7 +1,24 @@
 #include<iostream>
 #include<iomanip>
 
+#include "CollatzSeq.hpp"
+
+
 int main()
 {
-
+	int maxNum;
+	int maxNumSteps=0;
+	int numSteps;
+	for(unsigned int i=1; i<=1000000; ++i){
+		numSteps=0;
+		GenerateSeq(i,numSteps);
+		if (numSteps>maxNumSteps){
+			maxNum=i;
+			maxNumSteps=numSteps;
+		}
+	}
+	std::cout << maxNum <<std::endl;
+	std::cout << maxNumSteps <<std::endl;
+	return 0;
 }
+
